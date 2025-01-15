93c0c0c67d3f86e7ab168fb8483407178e09877e
linjiasong <jiasong.lin@shopcider.cn>
2025-01-15 10:57:43 +0800

    feat:UserLike 添加 gateway

---
+++ user/src/main/java/com/linjiasong/user/gateway/UserLikeGateWay.java
@@ -0,0 +1,8 @@
+package com.linjiasong.user.gateway;
+
+/**
+ * @author linjiasong
+ * @date 2025/1/15 上午10:53
+ */
+public interface UserLikeGateWay {
+}
---
+++ user/src/main/java/com/linjiasong/user/gateway/impl/UserLikeGateWayImpl.java
@@ -0,0 +1,13 @@
+package com.linjiasong.user.gateway.impl;
+
+import com.linjiasong.user.gateway.UserLikeGateWay;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author linjiasong
+ * @date 2025/1/15 上午10:54
+ */
+
+@Service
+public class UserLikeGateWayImpl implements UserLikeGateWay {
+}
