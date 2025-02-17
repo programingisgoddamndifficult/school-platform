78b9815f20f3bcb17590c6ba152f0281b9ddd146
linjiasong <jiasong.lin@shopcider.cn>
2025-02-17 17:14:55 +0800

    feat:用户注销页面逻辑

--- article/src/main/java/com/linjiasong/article/mq/service/AbstractMqService.java
+++ article/src/main/java/com/linjiasong/article/mq/service/AbstractMqService.java
@@ -1,6 +1,8 @@
 package com.linjiasong.article.mq.service;
 
 import com.linjiasong.article.gateway.ArticleBasicInfoGateway;
+import com.linjiasong.article.gateway.ArticleUserRecommendGateway;
+import com.linjiasong.article.gateway.ArticleUserWatchGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,9 @@ public abstract class AbstractMqService {
 
     @Autowired
     ArticleBasicInfoGateway articleBasicInfoGateway;
+
+    @Autowired
+    ArticleUserWatchGateway articleUserWatchGateway;
 
     abstract public void consume(String mqData);
 
--- article/src/main/java/com/linjiasong/article/mq/service/UserDeleteMqService.java
+++ article/src/main/java/com/linjiasong/article/mq/service/UserDeleteMqService.java
@@ -2,6 +2,7 @@ package com.linjiasong.article.mq.service;
 
 import com.alibaba.fastjson.JSON;
 import com.linjiasong.article.entity.ArticleBasicInfo;
+import com.linjiasong.article.entity.ArticleUserWatch;
 import com.linjiasong.article.mq.dto.UserDeleteDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,13 @@ public class UserDeleteMqService extends AbstractMqService {
             log.info(String.format("USER_DELETE 消费MQ消息失败 mqData:%s", mqData));
             throw new RuntimeException();
         }
+
+        if(!articleUserWatchGateway.deleteUserWatch(articleUserWatchGateway.getUserWatchList(userDeleteDTO.getUserId())
+                .stream().map(ArticleUserWatch::getId).toList())){
+            log.info(String.format("USER_DELETE 消费MQ消息失败 mqData:%s", mqData));
+            throw new RuntimeException();
+        }
+
         log.info(String.format("USER_DELETE 消费MQ消息成功 mqData:%s", mqData));
     }
 }
